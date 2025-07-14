b9f9121cc660bb620ce4e6183b75f1ddd7999743
Mahmut İsmail Öztürk <mahmutismail83@gmail.com>
2025-07-14 16:15:36 +0300

    Added tests

---
+++ PipelineWebApp.Tests/ContactModelTests.cs
@@ -0,0 +1,20 @@
+﻿using MyFirstWebApp.Pages;
+using Xunit;
+
+public class ContactModelTests
+{
+    [Fact]
+    public void OnPost_Sets_Message_Correctly()
+    {
+        // Arrange
+        var model = new ContactModel();
+        var testName = "Mahmut";
+        var testEmail = "mahmut@example.com";
+
+        // Act
+        model.OnPost(testName, testEmail);
+
+        // Assert
+        Assert.Equal($"Thank you, {testName}! We have received your email.", model.Message);
+    }
+}
